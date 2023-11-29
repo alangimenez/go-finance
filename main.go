7a4ccca0aab72b67afa9a25bbbdbdffa92e03905
Alan Gimenez <gimenezalan00@gmail.com>
2023-11-29 12:28:11 -0300

    add dummy endpoints

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// Respuesta JSON para el GET
+type GetResponse struct {
+	Key   string `json:"Key"`
+	Value string `json:"Value"`
+}
+
+func main() {
+	// Configurar los manejadores para los endpoints
+	http.HandleFunc("/handler", handler)
+
+	// Iniciar el servidor en el puerto 5050
+	port := 5050
+	fmt.Printf("Servidor escuchando en el puerto %d...\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		fmt.Println("Error al iniciar el servidor:", err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		handleGet(w, r)
+	case http.MethodPost:
+		handlePost(w, r)
+	default:
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+	}
+}
+
+func handleGet(w http.ResponseWriter, r *http.Request) {
+	// Manejar la solicitud GET
+	// Crear la respuesta JSON
+	response := GetResponse{
+		Key:   "Key",
+		Value: "Hola Alan",
+	}
+
+	// Codificar la respuesta como JSON
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		http.Error(w, "Error al codificar la respuesta JSON", http.StatusInternalServerError)
+		return
+	}
+}
+
+func handlePost(w http.ResponseWriter, r *http.Request) {
+	// Manejar la solicitud POST
+	// Decodificar el cuerpo JSON
+	var postData PostData
+	err := json.NewDecoder(r.Body).Decode(&postData)
+	if err != nil {
+		http.Error(w, "Error al decodificar el JSON", http.StatusBadRequest)
+		return
+	}
+
+	// Procesar los datos
+	fmt.Fprintf(w, "Handler POST en /handler. Mensaje recibido: %s\n", postData.Message)
+}
+
+type PostData struct {
+	Message string `json:"message"`
+}
