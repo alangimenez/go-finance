c82c1a81a56187e79c4e8a7c859c7bec06d64665
Alan Gimenez <gimenezalan00@gmail.com>
2023-12-16 19:37:21 -0300

    return in postman a list with name and TIR of each bond

--- main.go
+++ main.go
@@ -23,6 +23,25 @@ type GetResponse struct {
 	Value float64 `json:"Value"`
 }
 
+type Bond struct {
+	Simbolo      string
+	UltimoPrecio float64
+}
+
+type Quotes struct {
+	Quotes      []Bond    `bson:"quotes"`
+	Id          string    `bson:"_id"`
+	OtherQuotes PreDolars `bson:"otherQuotes"`
+}
+
+type PreDolars struct {
+	Quotes Dolars `bson:"quotes"`
+}
+
+type Dolars struct {
+	Mep float64 `bson:"dolarMep"`
+}
+
 func main() {
 	errEnv := godotenv.Load("config.env")
 	if errEnv != nil {
@@ -60,16 +79,16 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func handleGet(w http.ResponseWriter, r *http.Request) {
 	// Manejar la solicitud GET
 	// Crear la respuesta JSON
-	response := GetResponse{
+	/* response := GetResponse{
 		Key:   "TIR",
 		Value: 0.0,
-	}
+	} */
 
-	connectToMongo()
+	responsesList := connectToMongo()
 
 	// Codificar la respuesta como JSON
 	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(response)
+	err := json.NewEncoder(w).Encode(responsesList)
 	if err != nil {
 		http.Error(w, "Error al codificar la respuesta JSON", http.StatusInternalServerError)
 		return
@@ -104,13 +123,6 @@ func calcularNPV(tasaDescuento float64, cashFlow []float64) float64 {
 	}
 	var calculatedValuesWithInitialPayment []float64
 	calculatedValuesWithInitialPayment = append([]float64{cashFlow[0]}, calculatedValues...)
-
-	fmt.Printf("Array de calcularNPV \n")
-	for pos, value := range calculatedValuesWithInitialPayment {
-		if value != 0 {
-			fmt.Printf("Pos %d, value %f \n", pos, value)
-		}
-	}
 
 	sumatoria := 0.0
 	for _, valor := range calculatedValuesWithInitialPayment {
@@ -186,12 +198,12 @@ func diferenciaEnDias(fecha1, fecha2 time.Time) int {
 	fmt.Printf("Diferencia en días entre %v y %v: %d días\n", fecha1, fecha2, diferencia) */
 }
 
-func connectToMongo() {
+func connectToMongo() []GetResponse {
 	// Establecer información de conexión
 	uri, present := os.LookupEnv("MONGO_DB_URI")
 	if !present {
 		fmt.Printf("No esta definido el URI de Mongo.")
-		return
+		return []GetResponse{}
 	}
 	clientOptions := options.Client().ApplyURI(uri)
 
@@ -211,8 +223,10 @@ func connectToMongo() {
 	// Obtener una referencia a la colección
 	collection := client.Database("investment-project").Collection("cashflows")
 
+	excludedBonds := []string{"Test 202312101819"}
+
 	// Consultar todos los documentos en la colección
-	cursor, err := collection.Find(context.TODO(), bson.D{})
+	cursor, err := collection.Find(context.TODO(), bson.D{{Key: "ticket", Value: bson.D{{Key: "$nin", Value: excludedBonds}}}})
 	if err != nil {
 		// log.Fatal("Aca es el error")
 		log.Fatal(err)
@@ -221,6 +235,7 @@ func connectToMongo() {
 
 	// Recorrer los documentos y mostrarlos
 	var personas []Account
+	var tickets []string
 	for cursor.Next(context.Background()) {
 		var persona Account
 		err := cursor.Decode(&persona)
@@ -229,33 +244,70 @@ func connectToMongo() {
 		}
 		// fmt.Println(time.Parse("2006/01/02", persona.Start))
 		personas = append(personas, persona)
+		tickets = append(tickets, persona.Ticket)
 	}
 	fmt.Println("Documentos en la colección:")
 	fmt.Println(personas)
 
-	array := createArray(personas[0].Finish)
-	arrayTwo := addPaymentsToArray(
-		personas[0].DateOfPayment,
-		personas[0].AmountInterest,
-		personas[0].AmountAmortization,
-		array,
-	)
-	if arrayTwo == nil {
-		fmt.Printf("hola")
-	}
-	for pos, value := range arrayTwo {
-		if value != 0 {
-			fmt.Printf("Pos %d, value %f \n", pos, value)
-		}
-	}
-	/* tir := calcularTIR(arrayTwo)
-	tirAnual := tasaEfectivaAnual(tir / 100)
-	fmt.Printf("La tir es de %0.2f", tirAnual) */
+	collectionValues := client.Database("investment-project").Collection("lastvalues")
+	filter := bson.D{{Key: "name", Value: "final"}}
 
-	// cashflow := []float64{-10000.0, 5000.0, 9000.0}
-	tir := calculoTirByInterpolation(arrayTwo)
-	tirAnual := tasaEfectivaAnual(tir)
-	fmt.Printf("La tirAnual es de %f", tirAnual)
+	result := collectionValues.FindOne(context.TODO(), filter)
+	if result.Err() != nil {
+		log.Fatal(result.Err())
+	}
+
+	var quotesList Quotes
+	err = result.Decode(&quotesList)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Quotes: ", quotesList)
+
+	var responses []GetResponse
+
+	for _, bond := range personas {
+		fmt.Printf("Este es el bono %s \n", bond.Ticket)
+		array := createArray(bond.Finish)
+		fmt.Printf("Este es el bono %s after createArray. Length of array: %d \n", bond.Ticket, len(array))
+		actualPrice, errActualPrice := getActualPrice(quotesList.Quotes, bond.Ticket, quotesList.OtherQuotes.Quotes.Mep)
+		if errActualPrice != nil {
+			fmt.Printf("Error obteniendo el precio actual de %s \n", bond.Ticket)
+		}
+		fmt.Printf("Para bono %s precio %f", bond.Ticket, actualPrice)
+
+		arrayTwo := addPaymentsToArray(
+			bond.DateOfPayment,
+			bond.AmountInterest,
+			bond.AmountAmortization,
+			array,
+			actualPrice,
+		)
+		fmt.Printf("Este es el bono %s after addPaymentsToArray \n", bond.Ticket)
+		if arrayTwo == nil {
+			fmt.Printf("hola")
+		}
+		for pos, value := range arrayTwo {
+			if value != 0 {
+				fmt.Printf("Pos %d, value %f \n", pos, value)
+			}
+		}
+		/* tir := calcularTIR(arrayTwo)
+		tirAnual := tasaEfectivaAnual(tir / 100)
+		fmt.Printf("La tir es de %0.2f", tirAnual) */
+
+		// cashflow := []float64{-10000.0, 5000.0, 9000.0}
+		tir := calculoTirByInterpolation(arrayTwo)
+		tirAnual := tasaEfectivaAnual(tir)
+		fmt.Printf("La tirAnual es de %f", tirAnual)
+
+		response := GetResponse{
+			Key:   bond.Ticket,
+			Value: tirAnual,
+		}
+		responses = append(responses, response)
+	}
 
 	// Desconectar el cliente
 	err = client.Disconnect(context.Background())
@@ -263,6 +315,7 @@ func connectToMongo() {
 		log.Fatal(err)
 	}
 	fmt.Println("Conexión a MongoDB cerrada.")
+	return responses
 }
 
 type Account struct {
@@ -277,8 +330,10 @@ type Account struct {
 }
 
 func createArray(endDate time.Time) []float64 {
+	fmt.Printf("The endDate is %s", endDate)
 	// parsedEndTime := parseDate(endDate)
 	length := diferenciaEnDias(time.Now(), endDate)
+	fmt.Printf("The length is %d", length)
 	var array []float64
 	for i := 0; i < length; i++ {
 		array = append(array, 0)
@@ -286,7 +341,7 @@ func createArray(endDate time.Time) []float64 {
 	return array
 }
 
-func addPaymentsToArray(paymentDays []time.Time, paymentInterest, paymentAmortization, array []float64) []float64 {
+func addPaymentsToArray(paymentDays []time.Time, paymentInterest, paymentAmortization, array []float64, actualPrice float64) []float64 {
 	for i := 0; i < len(paymentDays); i++ {
 		// parsedEndTime := parseDate(paymentDays[i])
 		diffDays := diferenciaEnDias(time.Now(), paymentDays[i])
@@ -294,7 +349,7 @@ func addPaymentsToArray(paymentDays []time.Time, paymentInterest, paymentAmortiz
 			array[diffDays-1] = paymentInterest[i] + paymentAmortization[i]
 		}
 	}
-	array[0] = -100.00
+	array[0] = -actualPrice
 	return array
 }
 
@@ -335,4 +390,17 @@ func calculoTirByInterpolation(cashflow []float64) float64 {
 func interpolation(rate, npvPositive, npvNegative float64) float64 {
 	previousRate := rate - 0.000001
 	return previousRate + ((rate - previousRate) * (npvPositive / (npvPositive - npvNegative)))
+}
+
+func getActualPrice(quotesList []Bond, ticket string, mep float64) (float64, error) {
+	fmt.Printf("dolar mep %f", mep)
+	for _, p := range quotesList {
+		if p.Simbolo == ticket && ticket == "PMM29" {
+			return p.UltimoPrecio / mep, nil
+		}
+		if p.Simbolo == ticket {
+			return p.UltimoPrecio, nil
+		}
+	}
+	return 0.0, fmt.Errorf("No se encontró el ticket con el nombre %s", ticket)
 }
