b73a87b2fe8c7754080a88d0c9634d783a7941fc
AlanGimenez <gimenezalan00@gmail.com>
2024-01-10 20:50:23 -0300

    exclude MRECD from tir process

--- repositories/cashflowReposity.go
+++ repositories/cashflowReposity.go
@@ -13,7 +13,7 @@ import (
 var cashflowCollection *mongo.Collection = conexion.Client.Database("investment-project").Collection("cashflows")
 
 func GetAllCashflowsWithTickets() ([]model.Cashflow, []string) {
-	excludedBonds := []string{"Test 202312101819"}
+	excludedBonds := []string{"Test 202312101819", "MRECD"}
 
 	// Consultar todos los documentos en la colección
 	cursor, err := cashflowCollection.Find(context.TODO(), bson.D{{Key: "ticket", Value: bson.D{{Key: "$nin", Value: excludedBonds}}}})
